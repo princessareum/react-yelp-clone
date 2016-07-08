baba1088ccddd71c016296b6653b10444f9ee624
A-Reum Jung <areumjung1987@gmail.com>
2016-07-08 00:58:07 -0600

    added google map using google API

---
+++ src/views/Main/Container.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Map, {GoogleApiWrapper} from 'google-maps-react';
+
+export class Container extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello from the container
+        <Map
+          google={this.props.google} />
+      </div>
+    )
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: __GAPI_KEY__
+})(Container)
+
+// export default Container
