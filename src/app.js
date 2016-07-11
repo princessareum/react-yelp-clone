7c9fd99dc027bbc9d85f871f7962bdc61b1f5116
A-Reum Jung <areumjung1987@gmail.com>
2016-07-11 01:09:55 -0600

    cleaning the code

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import 'font-awesome/css/font-awesome.css'
-import './app.css'
+import 'font-awesome/css/font-awesome.css';
+import './app.css';
 
-import App from 'containers/App/App'
+import App from 'containers/App/App';
 
-import {browserHistory} from 'react-router'
-import makeRoutes from './routes'
+import {browserHistory} from 'react-router';
+import makeRoutes from './routes';
 
 const routes = makeRoutes()
 
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import {browserHistory, Router, Route, Redirect} from 'react-router'
+import React from 'react';
+import {browserHistory, Router, Route, Redirect} from 'react-router';
 
-import makeMainRoutes from './views/Main/routes'
+import makeMainRoutes from './views/Main/routes';
 
 export const makeRoutes = () => {
   const main = makeMainRoutes();
@@ -11,6 +11,6 @@ export const makeRoutes = () => {
       {main}
     </Route>
   )
-}
+};
 
 export default makeRoutes
